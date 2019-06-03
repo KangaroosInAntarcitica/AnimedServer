0a8473daa22dbf54d3411130803ceebd61ffb602
Andriy Dmytruk <Andriyd909@gmail.com>
2019-06-03 20:53:50 +0300

    Change mysql host

--- createDataBase.js
+++ createDataBase.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
 connection.connect();
-connection.query(`CREATE DATEBASE animed;`, (err,row, field) => {
+connection.query(`DROP DATABASE CREATE DATEBASE animed;`, (err,row, field) => {
     console.log(err);
 });
 
